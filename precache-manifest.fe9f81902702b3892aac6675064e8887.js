fedbceecc5fb4aef5b0dd41938400d98c505b82c
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-09-14 14:46:48 +0300

    Updates

---
+++ precache-manifest.fe9f81902702b3892aac6675064e8887.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8046d458446f3bea64f905b4516d3187",
+    "url": "/static-technical-doc-page/index.html"
+  },
+  {
+    "revision": "5b1193817510bca03e57",
+    "url": "/static-technical-doc-page/static/css/main.5d5b0a62.chunk.css"
+  },
+  {
+    "revision": "cdaa4088b266bc1ec9fd",
+    "url": "/static-technical-doc-page/static/js/2.b79285bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static-technical-doc-page/static/js/2.b79285bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b1193817510bca03e57",
+    "url": "/static-technical-doc-page/static/js/main.ac80d491.chunk.js"
+  },
+  {
+    "revision": "88bee45d4d25b3e89dd1",
+    "url": "/static-technical-doc-page/static/js/runtime-main.b7dfe62c.js"
+  }
+]);
